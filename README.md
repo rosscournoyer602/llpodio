ec04b071d18904f65d372a6c753888ed768c6054
Ross Cournoyer <rcoupvc@yahoo.com>
2017-04-12 20:35:29 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+//simple version
+
+respond to webhook generate link for placement test
+put link in podio item field
+respond to webhook grade placement test
+update item field to reflect recommended class
+respond to webhook generate link to pre-pop sign up form
+from sign-up onwards workflow remains unchanged
+
+
+//cool version
+
+respond to webhook generate link for placement test
+use ufhash to generate secure shortcode
+express server listens for url w/shortcode and brings up test
+respond to webhook grade placement test
+respond to webhook generate sign-up link
---
+++ server.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var Podio = require('podio-js').api;
+var bodyParser = require('body-parser');
+var https = require('https');
+var querystring = require('querystring');
+var app = express();
+var creds = require('./creds.json');
+
+var podio = new Podio({
+	authType: 'app',
+	clientId: creds.clientID,
+	clientSecret: creds.secret
+});
+
+app.get('/', function(req,res) {
+	//console.log(req);
+	res.send('halllo world');
+})
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.post('/', urlencodedParser, function(req, res) {
+
+	if (req.body.type === 'hook.verify') {
+
+		var hookID = req.body.hook_id;
+
+		var postBody = {
+			"code" : req.body.code
+		};
+
+		var options = {
+  		hostname: 'api.podio.com',
+  		port: 443,
+  		path: `/hook/${hookID}/verify/validate`,
+  		method: 'POST',
+  		headers: {
+              'Content-Type': 'application/json',
+          }
+		};
+
+		var verify = https.request(options, function(res) {
+			res.setEncoding('utf8');
+			res.on('data', function(chunk) {
+				console.log(chunk);
+			});
+		});
+
+		verify.write(postBody);
+		verify.end();
+	}
+
+	else {
+		console.log(Object.keys(req.body)[0]);
+	}
+})
+
+app.post('/placement', urlencodedParser, function(req, res) {
+	podio.authenticateWithApp(creds.ptAppID, creds.ptAppToken, function(err) {
+		if (err) {
+			throw new Error(err);
+		}
+		podio.isAuthenticated().then(function() {
+			var itemID = Object.keys(req.body)[0];
+			//console.log(itemID);
+			var token = podio.authObject.accessToken;
+			var path = `/app/${creds.ptAppID}/item/${itemID}?oauth_token=${token}`;
+			var ptFormPath = 'https://podio.com/webforms/18297059/1232341'
+			podio.request('GET', path).then(function(responseData) {
+				console.log(responseData.fields[1].values[0].value);
+				//get all student info, and format a string
+			}).catch(function(f) {
+				console.log(f)
+				})
+		})
+
+	})	
+})
+
+app.post('/signup',urlencodedParser, function(req, res) {
+
+})
+
+module.exports = app;
