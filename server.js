178a4f9158a37fb5bd2b5dbd7620018633d64b74
Ross Cournoyer <rcoupvc@yahoo.com>
2017-05-09 23:26:40 +0800

    removed them

--- server.js
+++ server.js
@@ -49,7 +49,6 @@ function encodeLookup(responseData, fieldName, defaultValue) {
 app.post('/grade', urlencodedParser, (req, res) => {
 	podio.isAuthenticated()
 		.then(function () {
-			console.log(req.body);
 			var appItemID = Object.keys(req.body)[0];
 			var token = podio.authObject.accessToken;
 			var itemPath = `/app/${process.env.ptAppID}/item/${appItemID}?oauth_token=${token}`;
@@ -68,7 +67,6 @@ app.post('/grade', urlencodedParser, (req, res) => {
 			var ps = Number(lookup(responseData, 'public-speaking-score'));
 
 			var placement = grader(grade, debXP, classXP, arg, ref, cs, ps);
-			console.log("Placement = " + placement)
 			var fieldPath = `/item/${itemID}/value/144010865`
 			var requestData = { value: placement }
 
